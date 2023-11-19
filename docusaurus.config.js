da8e67f125a1b5b9642ad01b09c1f4fd99146146
bonjour-npy <2366353971@qq.com>
2023-11-19 18:26:00 +0800

    remove edit this page button

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,8 +65,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/bonjour-npy/',
+          // editUrl: 'https://github.com/bonjour-npy/',
         },
         blog: {
           remarkPlugins: [math],
@@ -74,8 +73,7 @@ const config = {
           showReadingTime: false,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/bonjour-npy/',
+          // editUrl: 'https://github.com/bonjour-npy/',
         },
         theme: {
           // customCss: require.resolve('./src/css/custom.css'),
