344f95fc6c2aeb760c062101d79ed064dcec440c
bonjour-npy <2366353971@qq.com>
2023-12-19 13:41:06 +0800

    modify the homepage

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,30 +2,22 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
-import GUETLogo from '@site/static/img/GUET_logo.jpg';
-import SJTULogo from '@site/static/img/SJTU_logo.png';
+import GUETLogo from '@site/static/img/GUET_logo.png';
+import XDULogo from '@site/static/img/XDU_logo.png';
+import UESTCLogo from '@site/static/img/UESTC_logo.png';
 import ECNULogo from '@site/static/img/ECNU_logo.png';
-import XMULogo from '@site/static/img/XMU_logo.jpg';
+import XMULogo from '@site/static/img/XMU_logo.png';
 
 const FeatureList = [
-  {
-    title: '桂林电子科技大学',
-    img: GUETLogo,
-    description: (
-      <>
-        正德厚学，笃行致新
-      </>
-    ),
-  },
-  {
-    title: '上海交通大学',
-    img: SJTULogo,
-    description: (
-      <>
-        饮水思源，爱国荣校
-      </>
-    ),
-  },
+  // {
+  //   title: '桂林电子科技大学',
+  //   img: GUETLogo,
+  //   description: (
+  //     <>
+  //       正德厚学，笃行致新
+  //     </>
+  //   ),
+  // },
   {
     title: '华东师范大学',
     img: ECNULogo,
@@ -44,13 +36,32 @@ const FeatureList = [
       </>
     ),
   },
+  {
+    title: '西安电子科技大学',
+    img: XDULogo,
+    description: (
+      <>
+        红色西电，创新广研
+      </>
+    ),
+  },
+  {
+    title: '电子科技大学',
+    img: UESTCLogo,
+    description: (
+      <>
+        求实求真，大气大为
+      </>
+    ),
+  },
 ];
 
 function Feature({ img, title, description }) {
   return (
     <div className={clsx('col col--3')}>
       <div className="text--center">
-        <img src={img} alt="Image" style={{ width: '60%', height: 'auto' }} />
+        <img src={img} alt="Image" />
+        {/* <img src={img} alt="Image" style={{ width: 200, height: 'auto' }} /> */}
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
@@ -64,7 +75,7 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
+        <div className={clsx('row')}>
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
