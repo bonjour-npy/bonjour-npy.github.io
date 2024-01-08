b022c83e358ac57223e5e7042b0f9ef6d4572955
bonjour-npy <2366353971@qq.com>
2024-01-08 11:36:46 +0800

    modify the homepage

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import styles from './styles.module.css';
 
 import GUETLogo from '@site/static/img/GUET_logo.png';
+import HDULogo from '@site/static/img/HDU_logo.png';
 import XDULogo from '@site/static/img/XDU_logo.png';
 import UESTCLogo from '@site/static/img/UESTC_logo.png';
 import ECNULogo from '@site/static/img/ECNU_logo.png';
@@ -23,28 +24,64 @@ const FeatureList = [
     img: XDULogo,
     description: (
       <>
-        红色西电，创新广研
+        厚德、求真、砺学、笃行
       </>
     ),
   },
   {
-    title: '厦门大学',
-    img: XMULogo,
+    title: '杭州电子科技大学',
+    img: HDULogo,
     description: (
       <>
-        自强不息，止于至善
+        笃学、力行、守正、求新
       </>
     ),
   },
   {
-    title: '华东师范大学',
-    img: ECNULogo,
+    title: '桂林电子科技大学',
+    img: GUETLogo,
     description: (
       <>
-        求实创造，为人师表
+        正德厚学，笃行致新
       </>
     ),
   },
+  // {
+  //   title: '电子科技大学',
+  //   img: UESTCLogo,
+  //   description: (
+  //     <>
+  //       求实求真，大气大为
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: '西安电子科技大学',
+  //   img: XDULogo,
+  //   description: (
+  //     <>
+  //       红色西电，创新广研
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: '厦门大学',
+  //   img: XMULogo,
+  //   description: (
+  //     <>
+  //       自强不息，止于至善
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: '华东师范大学',
+  //   img: ECNULogo,
+  //   description: (
+  //     <>
+  //       求实创造，为人师表
+  //     </>
+  //   ),
+  // },
 ];
 
 function Feature({ img, title, description }) {
